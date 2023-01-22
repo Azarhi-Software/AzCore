90fb77cd709ee8267310f4683e2c90ed95dcf1d8
Jon Flowers <dev@azarhi.com>
2023-01-22 10:17:08 -0700

    Reorganized and added some more string extentions.

---
+++ Sources/AzCore/Extensions/StringProtocol+.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jon Flowers on 1/21/23.
+//
+
+import Foundation
+
+
+public extension StringProtocol
+{
+    var html2AttributedString: NSAttributedString?
+    {
+        Data(utf8).html2AttributedString
+    }
+    
+    var html2String: String
+    {
+        html2AttributedString?.string ?? ""
+    }
+}
